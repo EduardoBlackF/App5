341366401a46980463ec108790536b1c5603a1a1
EduardoBlackF <eduardo.black@tecsup.edu.pe>
2023-04-29 00:39:45 -0500

    Agregar archivos de proyecto.

---
+++ App5/App5/EmbeddedImage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms.Internals;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty (nameof(Source))]
+    
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        { 
+        
+            if (Source == null)
+             return null;
+            
+        return ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+        }
+    }
+}
